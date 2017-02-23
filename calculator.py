5c84ddaed27d19dadc5e743158c1986f94c7e32c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 16:04:41 -0800

    while loop to activate or end the calculator and conditional statements to call functions based on user input

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+"""# No setup
+repeat forever:
+    read input
+    tokenize input
+    if the first token is 'q'
+        quit
+    else
+        decide which math function to call based on first token
+"""
 """
 calculator.py
 
@@ -7,5 +16,25 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-
-# Your code goes here
+while True:
+    input = raw_input()
+    input_split = input.split(" ")
+    if input_split[0] == "q":
+        break
+    else:
+        if input_split[0] == "+":
+            add(input_split[1], input_split[2])
+        elif input_split[0] == "-":
+            subtract(input_split[1], input_split[2])
+        elif input_split[0] == "*":
+            multiply(input_split[1], input_split[2])
+        elif input_split[0] == "/":
+            divide(input_split[1], input_split[2])
+        elif input_split[0] == "square":
+            square(input_split[1])
+        elif input_split[0] == "cube":
+            cube(input_split[1])
+        elif input_split[0] == "pow":
+            power(input_split[1], input_split[2])
+        elif input_split[0] == "mod":
+            mod(input_split[1], input_split[2])
